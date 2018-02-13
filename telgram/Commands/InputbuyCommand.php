6021af0f9842a675519c94ffccfdf0140b95e498
JACK <123456@qq.com>
2018-02-13 12:47:23 +0800

    d

--- telgram/Commands/InputbuyCommand.php
+++ telgram/Commands/InputbuyCommand.php
@@ -25,15 +25,10 @@ class InputbuyCommand extends UserCommand
             $data=windowsinfo($chat_id,'发布购买',[['title'=>'    ','des'=>'请按照格式输入发布订单：/inputbuy 数量-单价-支付说明  (例如：  /inputbuy 1.2-55432)']]);
         }else{
             $text = explode('-',$text);
-            if(count($text) >= 3){
+            if(count($text) >= 2){
                 $num = (float)$text[0];
                 $price = (float)$text[1];
                 $allprice=$num*$price;
-                unset($text[0]);unset($text[1]);
-                $des="";
-                foreach ($text as $key => $value) {
-                    $des.=$value;
-                }
                 $cancel['action']='button';
                 $cancel['title']='发布购买';
                 $cancel['message']='取消发布';
@@ -54,7 +49,7 @@ class InputbuyCommand extends UserCommand
                 $orderid=1;
                 
                 
-                $data=windowsinfo($chat_id,'发布购买',[['title'=>'单价','des'=>$price],['title'=>'数量','des'=>$num],['title'=>'总价','des'=>$allprice],['title'=>'支付','des'=>$des]],[[['text'=>'确认','callback_data'=>"inorder-$orderid"],['text'=>'取消','callback_data'=>"button-取消发布成功"]]]);
+                $data=windowsinfo($chat_id,'发布购买',[['title'=>'单价','des'=>$price],['title'=>'数量','des'=>$num],['title'=>'总价','des'=>$allprice]],[[['text'=>'确认','callback_data'=>"inorder-$orderid"],['text'=>'取消','callback_data'=>"button-取消发布成功"]]]);
 
             }else{
                 $data=windowsinfo($chat_id,'发布购买',[['title'=>'    ','des'=>'格式不正确']]);
