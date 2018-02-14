fcfd0aaa4c386c3d91b5dc01331ad5e2e18a6c5f
Your Name <qiaopeng@qiaopengde.local>
2018-02-14 23:50:29 +0800

    d

--- telgram/hook.php
+++ telgram/hook.php
@@ -2,6 +2,9 @@
 // Load composer
 //file_put_contents("ok",json_encode([$_GET,$_SERVER,$_POST]));die;
 use Longman\TelegramBot\Request;
+use Longman\TelegramBot\DB;
+use PDO;
+
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__. '/set.php';
 //phpinfo();die;
@@ -36,7 +39,7 @@ $chat_id=$message['message']['chat']['id'];
 $text=$message['message']['text'];
 switch ($text) {
   case 'ud83cudf88u53d1u5e03u51fau552eud83dudc49':
-    Request::sendMessage(['chat_id' => $chat_id,'text'=> "/inputsell"]);
+    Request::sendMessage(inputsell($message));
     break;
   case 'ud83cudf88u53d1u5e03u8d2du4e70ud83dudc48':
     Request::sendMessage(['chat_id' => $chat_id,'text'=> "/inputbuy"]);
@@ -66,4 +69,94 @@ switch ($text) {
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
     // log telegram errors
  //   echo $e->getMessage();
+}
+
+
+
+function inputsell($message){
+        $chat_id=$message['message']['chat']['id'];
+        $text=$message['message']['text'];
+        $text=trim(str_replace("/inputsell","",$text));
+        if(empty($text)){
+            $data=windowsinfo($chat_id,'发布出售',[['title'=>'    ','des'=>'请按照格式输入发布订单：/inputsell 数量-单价-支付说明  (例如：  /inputsell 1.2-55432-支付宝账号 350177483@qq.com,谢谢！)']]);
+        }else{
+            $text = explode('-',$text);
+            if(count($text) >= 3){
+                $num = (float)$text[0];
+                $price = (float)$text[1];
+                $allprice=$num*$price;
+                unset($text[0]);unset($text[1]);
+                $des="";
+                foreach ($text as $key => $value) {
+                    $des.=$value;
+                }
+                $cancel['action']='button';
+                $cancel['title']='发布出售';
+                $cancel['message']='取消发布';
+                $cancel['chat_id']=$chat_id;
+
+
+
+                $orderinfo['action']='inputorder';
+                $orderinfo['num']=$num;
+                $orderinfo['price']=$price;
+                $orderinfo['allprice']=$allprice;
+                $orderinfo['des']=$des;
+                $orderinfo['chat_id']=$chat_id;
+
+                try {
+                    //余额检测
+
+                    $sth = DB::getPdo()->prepare('
+                        SELECT `walletId`,`socked`,`banlance`
+                        FROM `' . TB_USER . '`
+                        WHERE `id` = :id 
+                        LIMIT 1
+                    ');
+
+                    $sth->bindValue(':id', $chat_id);
+                    $sth->execute();
+                    $tempinfo = $sth->fetchAll(PDO::FETCH_ASSOC);
+                    foreach ($tempinfo as $key => $one) {
+                        $yueinfo = yue($tempinfo['walletId']);
+                        $walletbanlance=$yueinfo['balance']+$one['banlance'];
+                        if($walletbanlance >= $num){
+                            $sth = DB::getPdo()->prepare('
+                                INSERT INTO `' . "bitorder_temp" . '`
+                                (`buy_sell`, `seller_id`, `price`, `num`,`state`,`create_time`,`owner`,`des`)
+                                VALUES
+                                (:buy_sell, :seller_id, :price, :num,:state, :create_time, :owner,:des)
+                            ');
+                            $sth->bindValue(':buy_sell', '0');
+                            $sth->bindValue(':seller_id', $chat_id);
+                            $sth->bindValue(':price', $price);
+                            $sth->bindValue(':num', $num);
+                            $sth->bindValue(':state', '0');
+                            $sth->bindValue(':create_time', date("Y-m-d H:i:s",time()));
+                            $sth->bindValue(':owner', $chat_id);
+                            $sth->bindValue(':des', $des);
+
+                            $sth->execute();
+
+                            $sth = DB::getPdo()->prepare('SELECT LAST_INSERT_ID() as lastid ');
+                            $sth->execute();
+                            $lastid=$sth->fetchColumn();
+                            $data=windowsinfo($chat_id,'发布出售',[['title'=>'单价','des'=>$price],['title'=>'数量','des'=>$num],['title'=>'总价','des'=>$allprice],['title'=>'支付','des'=>$des]],[[['text'=>'确认','callback_data'=>"outorders-$lastid"],['text'=>'取消','callback_data'=>"canceltemporders-$lastid"]]]);
+                        }else{
+                            $data=windowsinfo($chat_id,'发布出售',[['title'=>'    ','des'=>'销售数量大于账户余额']]);
+                        }
+                    }
+                   
+                } catch (Exception $e) {
+                    throw new TelegramException($e->getMessage());
+                } 
+     
+               
+
+            }else{
+                $data=windowsinfo($chat_id,'发布出售',[['title'=>'    ','des'=>'格式不正确']]);
+            }
+            
+        }
+        return $data;
 }
