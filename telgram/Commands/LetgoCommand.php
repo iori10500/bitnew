7a5db27ed4be432f6fa1b824f6c0114074465d9a
Your Name <qiaopeng@qiaopengde.local>
2018-02-16 23:23:48 +0800

    d

---
+++ telgram/Commands/LetgoCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Longman\TelegramBot\Commands\UserCommands;
+
+use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Request;
+use Longman\TelegramBot\Entities\Keyboard;
+use Longman\TelegramBot\Entities\KeyboardButton;
+use Longman\TelegramBot\DB;
+
+class LetgoCommand extends UserCommand
+{
+    protected $name = 'balance';                      // Your command's name
+    protected $description = '余额查询,接收地址'; // Your command description
+    protected $usage = '/balance';                    // Usage of your command
+    protected $version = '1.0.0';                  // Version of your command
+
+    public function execute()
+    {
+        $message = $this->getMessage();            // Get Message object
+        $chat_id = $message->getChat()->getId();   // Get the current Chat ID
+        $datamessage= startwindows($chat_id,"start",[[['text'=>'🎈发布出售👉','callback_data'=>"nextmyorder"],['text'=>'🎈发布购买👈','callback_data'=>"nextmyorder"]],[['text'=>'🔄我要出售👉','callback_data'=>"nextmyorder"],['text'=>'🔄我要购买👈','callback_data'=>"nextmyorder"]],[['text'=>'👱‍♂️个人中心👱‍♂️','callback_data'=>"nextmyorder"],['text'=>'🙍邀请好友🙍','callback_data'=>"nextmyorder"]]]);
+        return Request::sendMessage($datamessage);        // Send message!
+    }
+}
