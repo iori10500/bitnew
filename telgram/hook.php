10e48541da5a685a769cb9e6350dab8ef9846bb3
Your Name <qiaopeng@qiaopengde.local>
2018-02-17 20:39:13 +0800

    d

--- telgram/hook.php
+++ telgram/hook.php
@@ -36,79 +36,81 @@ $telegram->addCommandsPaths($commands_paths);
 
 $telegram->handle();
 $message=json_decode(stripslashes(trim(file_get_contents("php://input"),chr(239).chr(187).chr(191))),true);
-$chat_id=$message['message']['chat']['id'];
-$text=$message['message']['text'];
-switch ($text) {
-  case 'ud83cudf88u53d1u5e03u51fau552eud83dudc49':   //inputsell
-    Request::sendMessage(windowsinfo($chat_id,'发布出售',[['title'=>'    ','des'=>'请按照格式输入发布订单：                   <strong>/inputsell 数量-单价</strong>(例如：  <em>/inputsell 1.2-55432</em>)']]));
-    break;
-  case 'ud83cudf88u53d1u5e03u8d2du4e70ud83dudc48':  //inputbuy
-    Request::sendMessage(windowsinfo($chat_id,'发布购买',[['title'=>'    ','des'=>'请按照格式输入发布订单：                   <strong>/inputbuy 数量-单价  </strong>(例如：  <em>/inputbuy 1.2-55432</em>)']]));
-    break;
-  case 'ud83dudd04u6211u8981u51fau552eud83dudc49':   //gosell
-    Request::sendMessage(getorder($chat_id,2,0));
-    break;
-  case 'ud83dudd04u6211u8981u8d2du4e70ud83dudc48':    //gobuy
-    Request::sendMessage(getorder($chat_id,3,0));
-    break;
-  case 'ud83dudc71u200du2642ufe0fu4e2au4ebau4e2du5fc3ud83dudc71u200du2642ufe0f':
-    Request::sendMessage(windowsinfo($chat_id,'个人中心',[],[[['text'=>'接收比特币','callback_data'=>"balance"]],[['text'=>'发送比特币','callback_data'=>"sendbitcoin"]],[['text'=>'订单中心','callback_data'=>"myorder"]],[['text'=>'设置收款','callback_data'=>"setcollections"]],[['text'=>'联系我们','callback_data'=>"contentus"]]]));
-    break;
-  case 'ud83dude4du9080u8bf7u597du53cbud83dude4d':
-    Request::sendMessage(windowsinfo($chat_id,'邀请好友',[['title'=>'    ','des'=>'邀请好友加入,您的下级每发生一笔订单,您将获得0.00001btc奖励']]));        // Send message!
-    $time=time(); 
-    Request::sendMessage( windowsinfo($chat_id,'邀请链接',[['title'=>$message['message']['chat']['username'],'des'=>"<a href='https://t.me/bitokbitbot?start=$chat_id&time=$time'>电币比特币c2c交易平台</a>"]]));        // Send message!
-    break;
-  default:
-    $pdo  = DB::getPdo();
-    $col_flag=$pdo->query('SELECT `col_flag` from user where id='.$chat_id)->fetchColumn();
-    if($col_flag){  
-      $systemcommde[]="invitenote";
-      $systemcommde[]="inputsell";
-      $systemcommde[]="inputbuy";
-      $systemcommde[]="balance";
-      $systemcommde[]="gobuy";
-      $systemcommde[]="gosell";
-      $systemcommde[]="invitenote";
-      $systemcommde[]="myinfo";
-      $systemcommde[]="myorder";
-      $systemcommde[]="button";
-      $systemcommde[]="inorder";
-      $systemcommde[]="outorder";
-      $systemcommde[]="outorders";
-      $systemcommde[]="nextmyorder";
-      $systemcommde[]="cancelorder";
-      $systemcommde[]="cancelpay";
-      $systemcommde[]="finishpay";
-      $systemcommde[]="adminorder";
-      $systemcommde[]="fangxingorder";
-      $systemcommde[]="gotorder";
-      $systemcommde[]="canceltemporder";
-      $systemcommde[]="canceltemporders";
-      $systemcommde[]="sendbitcoin";
-      $systemcommde[]="setcollections";
-      $systemcommde[]="contentus";
-      $iscommend=false;
-      foreach ($systemcommde as $key => $value) {
-        if(strpos($text,$value)){
-            $iscommend=true;
-            break;
-        }
-      }
-      if(!$iscommend){
-        $text=json_decode(file_get_contents("php://input"),true)['message']['text'];
-         $sth = $pdo->prepare('update user set collections_bak=:collections where id=:id ');
-          $sth->bindValue(':id', $chat_id);
-          $sth->bindValue(':collections', $text);
-          $sth->execute();
-          Request::sendMessage(windowsinfo($chat_id,'设置收款信息',[['title'=>'    ','des'=>$text]],[[['text'=>'确认','callback_data'=>"setcollections-1"],['text'=>'取消','callback_data'=>"setcollections-0"]]]));
-      }
-     
 
-    }
-    break;
+if(!empty($message['message'])){
+      $chat_id=$message['message']['chat']['id'];
+      $text=$message['message']['text'];
+      switch ($text) {
+        case 'ud83cudf88u53d1u5e03u51fau552eud83dudc49':   //inputsell
+          Request::sendMessage(windowsinfo($chat_id,'发布出售',[['title'=>'    ','des'=>'请按照格式输入发布订单：                   <strong>/inputsell 数量-单价</strong>(例如：  <em>/inputsell 1.2-55432</em>)']]));
+          break;
+        case 'ud83cudf88u53d1u5e03u8d2du4e70ud83dudc48':  //inputbuy
+          Request::sendMessage(windowsinfo($chat_id,'发布购买',[['title'=>'    ','des'=>'请按照格式输入发布订单：                   <strong>/inputbuy 数量-单价  </strong>(例如：  <em>/inputbuy 1.2-55432</em>)']]));
+          break;
+        case 'ud83dudd04u6211u8981u51fau552eud83dudc49':   //gosell
+          Request::sendMessage(getorder($chat_id,2,0));
+          break;
+        case 'ud83dudd04u6211u8981u8d2du4e70ud83dudc48':    //gobuy
+          Request::sendMessage(getorder($chat_id,3,0));
+          break;
+        case 'ud83dudc71u200du2642ufe0fu4e2au4ebau4e2du5fc3ud83dudc71u200du2642ufe0f':
+          Request::sendMessage(windowsinfo($chat_id,'个人中心',[],[[['text'=>'接收比特币','callback_data'=>"balance"]],[['text'=>'发送比特币','callback_data'=>"sendbitcoin"]],[['text'=>'订单中心','callback_data'=>"myorder"]],[['text'=>'设置收款','callback_data'=>"setcollections"]],[['text'=>'联系我们','callback_data'=>"contentus"]]]));
+          break;
+        case 'ud83dude4du9080u8bf7u597du53cbud83dude4d':
+          Request::sendMessage(windowsinfo($chat_id,'邀请好友',[['title'=>'    ','des'=>'邀请好友加入,您的下级每发生一笔订单,您将获得0.00001btc奖励']]));        // Send message!
+          $time=time(); 
+          Request::sendMessage( windowsinfo($chat_id,'邀请链接',[['title'=>$message['message']['chat']['username'],'des'=>"<a href='https://t.me/bitokbitbot?start=$chat_id&time=$time'>电币比特币c2c交易平台</a>"]]));        // Send message!
+          break;
+        default:
+          $pdo  = DB::getPdo();
+          $col_flag=$pdo->query('SELECT `col_flag` from user where id='.$chat_id)->fetchColumn();
+          if($col_flag){  
+            $systemcommde[]="invitenote";
+            $systemcommde[]="inputsell";
+            $systemcommde[]="inputbuy";
+            $systemcommde[]="balance";
+            $systemcommde[]="gobuy";
+            $systemcommde[]="gosell";
+            $systemcommde[]="invitenote";
+            $systemcommde[]="myinfo";
+            $systemcommde[]="myorder";
+            $systemcommde[]="button";
+            $systemcommde[]="inorder";
+            $systemcommde[]="outorder";
+            $systemcommde[]="outorders";
+            $systemcommde[]="nextmyorder";
+            $systemcommde[]="cancelorder";
+            $systemcommde[]="cancelpay";
+            $systemcommde[]="finishpay";
+            $systemcommde[]="adminorder";
+            $systemcommde[]="fangxingorder";
+            $systemcommde[]="gotorder";
+            $systemcommde[]="canceltemporder";
+            $systemcommde[]="canceltemporders";
+            $systemcommde[]="sendbitcoin";
+            $systemcommde[]="setcollections";
+            $systemcommde[]="contentus";
+            $iscommend=false;
+            foreach ($systemcommde as $key => $value) {
+              if(strpos($text,$value)){
+                  $iscommend=true;
+                  break;
+              }
+            }
+            if(!$iscommend){
+              $text=json_decode(file_get_contents("php://input"),true)['message']['text'];
+               $sth = $pdo->prepare('update user set collections_bak=:collections where id=:id ');
+                $sth->bindValue(':id', $chat_id);
+                $sth->bindValue(':collections', $text);
+                $sth->execute();
+                Request::sendMessage(windowsinfo($chat_id,'设置收款信息',[['title'=>'    ','des'=>$text]],[[['text'=>'确认','callback_data'=>"setcollections-1"],['text'=>'取消','callback_data'=>"setcollections-0"]]]));
+            }
+           
+
+          }
+          break;
+      }
 }
-
 
 
 
