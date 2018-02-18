f39e516bc0d4939812ee3e161dc1ada28556a912
Your Name <qiaopeng@qiaopengde.local>
2018-02-18 22:15:00 +0800

    d

--- telgram/autorun.php
+++ telgram/autorun.php
@@ -40,3 +40,99 @@ try {
     $conn->query('ROLLBACK');
 }
 $conn->close();
+
+
+//----------------------------------机器人------------------------------
+
+
+$tempbuy=file_get_contents("https://api-otc.huobi.pro/v1/otc/trade/list/public?coinId=1&tradeType=1&currentPage=1&payWay=&country=&merchant=1&online=1&range=0");
+$temp=json_decode($tempbuy,true);
+$price=$temp['data']['price'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} 
+$shoukuanmark=[
+    '微信： QP350166483 ',
+    '支付宝：350166483@qq.com  谯鹏',
+    '支付宝：18080520660  杨青山',
+    '支付宝：1107969784@qq.com  谢天明',
+    '支付宝：15072466127  张武宗',
+    '支付宝：18190117297  吴文丰',
+    '支付宝：350166483@qq.com  谯鹏',
+    '微信：id_tianming',//谢天明
+    '微信：zy940814',//杨青山
+    '微信：zwz_07',//张武宗
+    '微信：wendyfe12478',//吴文丰
+];
+$time=time();
+try {
+    $conn->query('BEGIN');
+    $conn->query('set names utf8');
+    //--------------------------------------------------------------------------------------
+    $result = $conn->query('SELECT count(*) as number from bitorder where state=0 and istest=1 and buy_sell=1');
+
+    while($row = $result->fetch_assoc()) {
+        if(1|| $row['number'] < 10){
+            mysqli_query($conn,'DELETE FROM bitorder WHERE state=0 and istest=1 and buy_sell=1'); 
+            $buyorder=[];
+            unset($temp);
+            for($i=0;$i<10;$i++){
+                $temp['buy_sell']=1;
+                $temp['seller_id']=528254045;
+                $temp['price']=rand((int)($price-1000),(int)($price-500));
+                $temp['num']=rand(1,1000)/1000;
+                $temp['state']=0;
+                $temp['owner']=528254045;
+                $temp['des']=$shoukuanmark[rand(0,10)];
+                $temp['istest']=1;    
+            }
+            foreach ($buyorder as $key => $value) {
+                mysqli_query($conn,'
+                        INSERT INTO `' . "bitorder" . '`
+                        (`buy_sell`, `price`,`seller_id`, `num`,`state`,`owner`,`des`,`istest`)
+                        VALUES
+                        (1, '.$value['price'].', '.$value['seller_id'].', '.$value['num'].',0, '.$value['owner'].',"'.$value['des'].'",'.$value['istest'].')
+                    ');
+            }
+        }
+    }
+     $result = $conn->query('SELECT count(*) as number from bitorder where state=0 and istest=1 and buy_sell=0');
+     while($row = $result->fetch_assoc()) {
+        if(1 || $row['number'] < 10){
+            mysqli_query($conn,'DELETE FROM bitorder WHERE state=0 and istest=1 and buy_sell=1'); 
+            $buyorder=[];
+            unset($temp);
+            for($i=0;$i<10;$i++){
+                $temp['buy_sell']=0;
+                $temp['seller_id']=528254045;
+                $temp['price']=rand((int)($price-500),(int)($price+500));
+                $temp['num']=rand(1,1000)/1000;
+                $temp['state']=0;
+                $temp['owner']=528254045;
+                $temp['des']="     ";
+                $temp['istest']=1;    
+            }
+            foreach ($buyorder as $key => $value) {
+                mysqli_query($conn,'
+                        INSERT INTO `' . "bitorder" . '`
+                        (`buy_sell`, `price`,`seller_id`, `num`,`state`,`owner`,`des`,`istest`)
+                        VALUES
+                        (1, '.$value['price'].', '.$value['seller_id'].', '.$value['num'].',0, '.$value['owner'].',"'.$value['des'].'",'.$value['istest'].')
+                    ');
+            }
+        }
+    }
+    //--------------------------------------------------------------------------------------
+    $conn->query('COMMIT');
+    
+} catch (Exception $e) {
+    $conn->query('ROLLBACK');
+}
+$conn->close();
+
+
+
+
