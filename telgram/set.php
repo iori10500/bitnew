f66beaea02aa8baa7b4fb6740ede5d7611629ac9
JACK <123456@qq.com>
2018-02-19 14:58:57 +0800

    d

--- telgram/set.php
+++ telgram/set.php
@@ -92,7 +92,8 @@ function windowsinfo($chat_id,$title,$data,$button=false){
     \n</pre>";
     foreach($data as $one){
         $flag=empty(trim($one['title']))?"":":";
-        $text.=("<code style='color:#00b5f6'>".$one['title']."</code>".$flag." ".$one['des'].'                                                                                        ');
+        $text.=("<code style='color:#00b5f6'>".$one['title']."</code>".$flag." ".$one['des'].'<pre>
+    \n</pre>');
     }
     $buttoninfo['text']=$text;
     if($button){
