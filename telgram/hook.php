d1f7729c2bf9b879e73220678a826041f4a23444
Your Name <qiaopeng@qiaopengde.local>
2018-02-20 20:43:44 +0800

    d

--- telgram/hook.php
+++ telgram/hook.php
@@ -70,8 +70,17 @@ if(!empty($message['message'])){
           $time=time(); 
           Request::sendMessage( windowsinfo($chat_id,'邀请链接',[['title'=>isset($message['message']['chat']['username'])?$message['message']['chat']['username']:$message['message']['chat']['first_name'],'des'=>"<a href='https://t.me/bitokbitbot?start=$chat_id&time=$time'>电币比特币c2c交易平台</a>"]]));        // Send message!
           if(0 && $chat_id == 528254045){
-            Request::sendMessage(windowsinfo(361550111,'订单信息',[['title'=>'    ','des'=>'尊敬的用户您好！我们发现您存在伪交易，现已回退您的订单至投诉状态，如有任何问题请邮件告知我们，邮件内容>请详细备注订单号，问题说明。我们将在2个工作日
-            内尽快给与答复，祝你交易愉快！谢谢']]));
+           // Request::sendMessage(windowsinfo(361550111,'订单信息',[['title'=>'    ','des'=>'尊敬的用户您好！我们发现您存在伪交易，现已回退您的订单至投诉状态，如有任何问题请邮件告知我们，邮件内容>请详细备注订单号，问题说明。我们将在2个工作日内尽快给与答复，祝你交易愉快！谢谢']]));
+            $sth = DB::getPdo()->prepare('
+                    SELECT id
+                    FROM `' . "user" . '`
+                    WHERE  1 ');
+            $sth->execute();
+            $users = $sth->fetchAll(PDO::FETCH_ASSOC);
+            $news=file_get_contents("news");
+            foreach ($users as $key => $value) {
+              Request::sendMessage(windowsinfo(361550111,'比特快讯',[['title'=>'    ','des'=>$news]]));
+            }
           }
           break;
         default:
