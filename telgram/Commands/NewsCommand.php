a51f8d22eb70873f1769c33bf2fd0d7b4b9d11b8
Your Name <qiaopeng@qiaopengde.local>
2018-03-11 20:49:04 +0800

    d

--- telgram/Commands/NewsCommand.php
+++ telgram/Commands/NewsCommand.php
@@ -22,46 +22,49 @@ class NewsCommand extends UserCommand
 
         $chat_id = $message->getChat()->getId();   // Get the current Chat ID
         $userid =  $message->getFrom()->getId();
-        if(file_exists("users.js")){
-          $users=file_get_contents("users.js");
-          $users=json_decode($users,true);
-           $news=file_get_contents("news");
-           $sendresult=[];
-           for($i=0;$i<50;$i++){
-                $tempuser = array_pop($users);
-                if($tempuser){
-                     /*        $buttoninfo['chat_id']=$tempuser;
-                      $buttoninfo['photo']='http://telgram.bitneworld.com/app/xuanchuan.png';
-                      Request::sendPhoto($buttoninfo);        // Send me
-                    */
-                    $temp = Request::sendMessage(windowsinfo(528254045,'比特快讯',[['title'=>'    ','des'=>$news]]));
-                    $sendresult[]=$temp->ok;
-                }else{
-                    break;
-                }
-                
-           }
-           file_put_contents("users.js", json_encode($users));
-           if($i==50){
-                $buttoninfo['chat_id']=$chat_id;
-                $buttoninfo['parse_mode']='HTML';
-                $buttoninfo['text']="/news@bitokbitbot";
-                Request::sendMessage($buttoninfo);        // Send message!
-           }
-           $sendresult=json_encode($sendresult);
-        }else{
-            $sendresult="init news users";
-           $sth = DB::getPdo()->prepare('
-                SELECT id
-                FROM `' . "user" . '`
-                WHERE  1 ');
-          $sth->execute();
-          $users = $sth->fetchAll(PDO::FETCH_ASSOC);
-          $userss=[];
-          foreach ($users as $key => $value) {
-            $userss[]=$value['id'];
-          }
-          file_put_contents("users.js", json_encode($userss));
+        if($userid == 528254045){
+            if(file_exists("users.js")){
+              $users=file_get_contents("users.js");
+              $users=json_decode($users,true);
+               $news=file_get_contents("news");
+               $sendresult=[];
+               for($i=0;$i<50;$i++){
+                    $tempuser = array_pop($users);
+                    if($tempuser){
+                         /*        $buttoninfo['chat_id']=$tempuser;
+                          $buttoninfo['photo']='http://telgram.bitneworld.com/app/xuanchuan.png';
+                          Request::sendPhoto($buttoninfo);        // Send me
+                        */
+                        $temp = Request::sendMessage(windowsinfo(528254045,'比特快讯',[['title'=>'    ','des'=>$news]]));
+                        $sendresult[]=$temp->ok;
+                    }else{
+                        break;
+                    }
+                    
+               }
+               file_put_contents("users.js", json_encode($users));
+               if($i==50){
+                    $buttoninfo['chat_id']=$chat_id;
+                    $buttoninfo['parse_mode']='HTML';
+                    $buttoninfo['text']="/news@bitokbitbot";
+                    Request::sendMessage($buttoninfo);        // Send message!
+               }
+               $sendresult=json_encode($sendresult);
+            }else{
+                $sendresult="init news users";
+               $sth = DB::getPdo()->prepare('
+                    SELECT id
+                    FROM `' . "user" . '`
+                    WHERE  1 ');
+              $sth->execute();
+              $users = $sth->fetchAll(PDO::FETCH_ASSOC);
+              $userss=[];
+              foreach ($users as $key => $value) {
+                $userss[]=$value['id'];
+              }
+              file_put_contents("users.js", json_encode($userss));
+            }
+
         }
         $buttoninfo['chat_id']=$chat_id;
         $buttoninfo['parse_mode']='HTML';
