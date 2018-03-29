1e22bd324638ff5549c4e175295d97b573e6aa7a
Your Name <qiaopeng@qiaopengde.local>
2018-03-29 11:54:01 +0800

    d

--- telgram/src/Commands/SystemCommands/CallbackqueryCommand.php
+++ telgram/src/Commands/SystemCommands/CallbackqueryCommand.php
@@ -290,7 +290,7 @@ class CallbackqueryCommand extends SystemCommand
                 $userinfo = $sth->fetchAll(PDO::FETCH_ASSOC);
                 $walletId=$userinfo[0]['walletid'];
                 $yueinfo = yue($walletId);
-                $datamessage=windowsinfo($sendtomessageid,'地址余额',[['title'=>'账户余额','des'=>$yueinfo['balance']+$userinfo[0]['banlance']],['title'=>'接收地址','des'=>$yueinfo['address']]]);    
+                $datamessage=windowsinfo($sendtomessageid,'地址余额',[['title'=>'账户余额','des'=>$yueinfo['balance']+$userinfo[0]['banlance']],['title'=>'接收地址','des'=>$yueinfo['address']],['title'=>'充值说明','des'=>"充值1个交易确认即到账,充值无上下限值"]]);    
                 Request::sendMessage($datamessage);        // Send me
                 $buttoninfo['chat_id']=$sendtomessageid;
                 $buttoninfo['photo']='http://chart.apis.google.com/chart?chs=150x150&cht=qr&chld=L|0&chl='.urlencode($yueinfo['address']);
